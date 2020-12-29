210757c1c4e21a76aa2e109a679c35dc0468179e
Sai <saimuppiri@gmail.com>
2020-12-29 18:04:07 -0500

    [Lab 0]
    Added function calculate_apple_price that takes a u32 and returns a u32.

--- tut/0-rustlings/exercises/test1.rs
+++ tut/0-rustlings/exercises/test1.rs
@@ -7,10 +7,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn ..... {
+fn calculate_apple_price(num_apple: u32) -> u32 {
+    if num_apple > 40 {
+        num_apple
+    } else {
+        num_apple * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
