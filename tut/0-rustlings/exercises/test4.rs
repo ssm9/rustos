2046337a380b44ffc023e96528d1aa0a707fb1d6
Sai <saimuppiri@gmail.com>
2020-12-30 12:25:16 -0500

    [Lab 0]
    test4.rs - Created macros module with a my_macro that matches "world" and any other string.

--- tut/0-rustlings/exercises/test4.rs
+++ tut/0-rustlings/exercises/test4.rs
@@ -5,7 +5,17 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+mod macros {
+    #[macro_export]
+    macro_rules! my_macro {
+        ("world") => {
+            "Hello world!";
+        };
+        ($val:expr) => {
+            format!("You said {}", $val);
+        }
+    }
+}
 
 fn main() {
     if my_macro!("world!") != "Hello world!" {
